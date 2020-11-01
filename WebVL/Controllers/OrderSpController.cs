5d3f36a36a208f1a4b04358691106206a2fa2b84
Alars99 <hoanglong291099@gmail.com>
2020-11-02 00:19:26 +0700

    FixLayout_Tien

--- WebVL/Controllers/OrderSpController.cs
+++ WebVL/Controllers/OrderSpController.cs
@@ -15,68 +15,102 @@ namespace WebVL.Controllers
 
         public ActionResult Index()
         {
-            var order = from tt in db.Orders select tt;
-            return View(order);
+            if (Session["TaikhoanAdmin"] == null)
+            {
+                return RedirectToAction("LoginAdmin", "AdHome");
+            }
+            else
+            {
+                var order = from tt in db.Orders select tt;
+                return View(order);
+            }
         }
         public ActionResult Delete(int id)
         {
-            var cargo = db.Orders.First(m => m.IdBill == id);
-            return View(cargo);
+            if (Session["TaikhoanAdmin"] == null)
+            {
+                return RedirectToAction("LoginAdmin", "AdHome");
+            }
+            else
+            {
+                var cargo = db.Orders.First(m => m.IdBill == id);
+                return View(cargo);
+            }
         }
         public ActionResult Edit(int id)
         {
-            var Eb = db.Orders.First(m => m.IdBill == id);
-            return View(Eb);
+            if (Session["TaikhoanAdmin"] == null)
+            {
+                return RedirectToAction("LoginAdmin", "AdHome");
+            }
+            else
+            {
+                var Eb = db.Orders.First(m => m.IdBill == id);
+                return View(Eb);
+            }
         }
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            var cargo = db.Orders.Where(m => m.IdBill == id).First();
-            db.Orders.Remove(cargo);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            if (Session["TaikhoanAdmin"] == null)
+            {
+                return RedirectToAction("LoginAdmin", "AdHome");
+            }
+            else
+            {
+                var cargo = db.Orders.Where(m => m.IdBill == id).First();
+                db.Orders.Remove(cargo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
         }
 
 
         [HttpPost]
         public ActionResult Edit(Order order, int id, FormCollection collection)
         {
-            
-            var cargo = db.Orders.First(m => m.IdBill == id);
-            var namecargo = collection["Cargo"];
-            var daybook = collection["DayBooks"];
-            var namecus = collection["NameCus"];
-            var count = collection["Count"];
-            var price = collection["Price"];
-            var address = collection["Address"];
-            var status = collection["Status"];
-            cargo.IdBill = id;
-            if (string.IsNullOrEmpty(namecargo))
+            if (Session["TaikhoanAdmin"] == null)
             {
-                ViewData["Error"] = "Bill id is null";
+                return RedirectToAction("LoginAdmin", "AdHome");
             }
             else
             {
+                var cargo = db.Orders.First(m => m.IdBill == id);
+                var namecargo = collection["Cargo"];
+                var daybook = collection["DayBooks"];
+                var namecus = collection["NameCus"];
+                var count = collection["Count"];
+                var price = collection["Price"];
+                var address = collection["Address"];
+                var status = collection["Status"];
+                cargo.IdBill = id;
+                if (string.IsNullOrEmpty(namecargo))
+                {
+                    ViewData["Error"] = "Bill id is null";
+                }
+                else
+                {
 
-                cargo.Cargo= namecargo;
-                cargo.Count = count;
-                cargo.Address = address;
-                cargo.DayBooks = daybook;
-                cargo.NameCus = namecus;
-                cargo.Price = price;
-                cargo.Status = Int32.Parse(status);
+                    cargo.Cargo = namecargo;
+                    cargo.Count = count;
+                    cargo.Address = address;
+                    cargo.DayBooks = daybook;
+                    cargo.NameCus = namecus;
+                    cargo.Price = price;
+                    cargo.Status = Int32.Parse(status);
 
-                UpdateModel(namecargo);
-                UpdateModel(price);
-                UpdateModel(daybook);
-                UpdateModel(address);
-                UpdateModel(namecus);
-                UpdateModel(count);
-                UpdateModel(status);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                    UpdateModel(namecargo);
+                    UpdateModel(price);
+                    UpdateModel(daybook);
+                    UpdateModel(address);
+                    UpdateModel(namecus);
+                    UpdateModel(count);
+                    UpdateModel(status);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return this.Edit(id);
             }
-            return this.Edit(id);
         }
     }
 }
