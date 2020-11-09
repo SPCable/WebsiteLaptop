08effc2de8a2fe351a056f09ba15412ab1d2d221
Alars99 <hoanglong291099@gmail.com>
2020-11-09 16:31:03 +0700

    fix

---
+++ WebVL/Models/ShoppingCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebVL.Models
+{
+    public class ShoppingCard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public double Price { get; set; }
+        public int Number { get; set; }
+        public double Total { get { return Number * Price;  } }
+    }
+}
